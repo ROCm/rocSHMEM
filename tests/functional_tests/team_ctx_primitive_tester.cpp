4f3fb659e6089e6ddbef163d0d428857d25fc284
avinashkethineedi <avinash.kethineedi@amd.com>
2025-03-12 20:17:37 +0000

    Update start time in team primitive tests

--- tests/functional_tests/team_ctx_primitive_tester.cpp
+++ tests/functional_tests/team_ctx_primitive_tester.cpp
@@ -34,13 +34,22 @@ rocshmem_team_t team_primitive_world_dup;
 __global__ void TeamCtxPrimitiveTest(int loop, int skip, long long int *start_time,
                                      long long int *end_time, char *source,
                                      char *dest, int size, TestType type,
-                                     ShmemContextType ctx_type,
+                                     ShmemContextType ctx_type, int wf_size,
                                      rocshmem_team_t team) {
   __shared__ rocshmem_ctx_t ctx;
   int wg_id = get_flat_grid_id();
+  int t_id  = get_flat_block_id();
+  int wf_id = t_id / wf_size;
 
   rocshmem_wg_init();
   rocshmem_wg_team_create_ctx(team, ctx_type, &ctx);
+
+  /**
+   * Shared array to capture the start time for each wavefront
+   * Max threads per block = 1024, wavefront size = 64 (in most GPUs)
+   * Maximum array size required = 1024/64 = 16
+   */
+  __shared__ long long int wf_start_time[16];
 
   /**
    * Calculate start index for each thread within the grid
@@ -54,7 +63,8 @@ __global__ void TeamCtxPrimitiveTest(int loop, int skip, long long int *start_ti
       // Ensures all RMA calls from the skip loops are completed
       rocshmem_ctx_quiet(ctx);
       __syncthreads();
-      start_time[wg_id] = wall_clock64();
+      // Capture the start time of each wavefront to identify the earliest one
+      wf_start_time[wf_id] = wall_clock64();
     }
     switch (type) {
       case TeamCtxGetTestType:
@@ -76,7 +86,23 @@ __global__ void TeamCtxPrimitiveTest(int loop, int skip, long long int *start_ti
 
   rocshmem_ctx_quiet(ctx);
 
+  /**
+   * End time of the last wavefront is recorded by overwriting
+   * the value previously set by earlier wavefronts.
+   */
   end_time[wg_id] = wall_clock64();
+
+  // Find the earliest start time
+  int num_wfs = (get_flat_block_size() - 1 ) / wf_size + 1;
+  for (int i = num_wfs; i > 0; i >>= 1 ) {
+    if(t_id < i) {
+      wf_start_time[t_id] = min(wf_start_time[t_id] , wf_start_time[t_id + i]);
+    }
+  }
+
+  if (t_id == 0) {
+    start_time[wg_id] = wf_start_time[0];
+  }
 
   rocshmem_wg_ctx_destroy(&ctx);
   rocshmem_wg_finalize();
@@ -132,7 +158,7 @@ void TeamCtxPrimitiveTester::launchKernel(dim3 gridSize, dim3 blockSize,
 
   hipLaunchKernelGGL(TeamCtxPrimitiveTest, gridSize, blockSize, shared_bytes,
                      stream, loop, args.skip, start_time, end_time, source,
-                     dest, size, _type, _shmem_context,
+                     dest, size, _type, _shmem_context, wf_size,
                      team_primitive_world_dup);
 
   num_msgs = (loop + args.skip) * gridSize.x * blockSize.x;
