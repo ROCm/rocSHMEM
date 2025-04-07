dc61fb6102d30af41bc0dae6d9eb7ba40bc75394
Yiltan <yiltan@amd.com>
2025-04-07 12:55:32 -0500

    Added error checking to verbs functions

--- src/gpu_ib/backend_ib.cpp
+++ src/gpu_ib/backend_ib.cpp
@@ -21,6 +21,7 @@
  *****************************************************************************/
 
 #include "backend_ib.hpp"
+#include "gpuib_macros.inl"
 
 #include <endian.h>
 #include <mpi.h>
@@ -83,9 +84,7 @@ GPUIBBackend::GPUIBBackend(MPI_Comm comm) : Backend() {
   NET_CHECK(MPI_Comm_rank(gpu_ib_comm_world, &my_pe));
 
   /* Initialize the host interface */
-  host_interface =
-      //new HostInterface(hdp_proxy_.get(), gpu_ib_comm_world, &heap);
-      new HostInterface(nullptr, gpu_ib_comm_world, &heap);
+  host_interface = new HostInterface(nullptr, gpu_ib_comm_world, &heap);
 
   /*
    * Construct default host context independently of the
