a6da7c32bb11f33046a6c82f2c607b1698f1486f
Brandon Potter <brandon.potter@amd.com>
2025-04-15 11:27:47 -0500

    Add device-side print

--- src/gpu_ib/queue_pair.cpp
+++ src/gpu_ib/queue_pair.cpp
@@ -141,6 +141,34 @@ __device__ void QueuePair::post_wqe_rma(int pe, int32_t size, uintptr_t *laddr, 
     update_wqe_ce(num_wqes);
     ring_doorbell(db_val);
   }
+
+  union mlx5_segment {
+    mlx5_wqe_ctrl_seg ctrl_seg;
+    mlx5_wqe_raddr_seg raddr_seg;
+    mlx5_wqe_data_seg data_seg;
+    mlx5_wqe_atomic_seg atomic_seg;
+  };
+
+  mlx5_segment *base_ptr = reinterpret_cast<mlx5_segment*>(sq_buf);
+  size_t segment_offset = my_sq_index * 4;
+  const uint8_t *d = reinterpret_cast<const uint8_t*>(&base_ptr[segment_offset]);
+  printf("WQE: "
+   "%02x %02x %02x %02x %02x %02x %02x %02x "
+   "%02x %02x %02x %02x %02x %02x %02x %02x "
+   "%02x %02x %02x %02x %02x %02x %02x %02x "
+   "%02x %02x %02x %02x %02x %02x %02x %02x "
+   "%02x %02x %02x %02x %02x %02x %02x %02x "
+   "%02x %02x %02x %02x %02x %02x %02x %02x "
+   "%02x %02x %02x %02x %02x %02x %02x %02x "
+   "%02x %02x %02x %02x %02x %02x %02x %02x\n",
+    d[0],  d[1],  d[2],  d[3],  d[4],  d[5],  d[6],  d[7],
+    d[8],  d[9], d[10], d[11], d[12], d[13], d[14], d[15],
+   d[16], d[17], d[18], d[19], d[20], d[21], d[22], d[23],
+   d[24], d[25], d[26], d[27], d[28], d[29], d[30], d[31],
+   d[32], d[33], d[34], d[35], d[36], d[37], d[38], d[39],
+   d[40], d[41], d[42], d[43], d[44], d[45], d[46], d[47],
+   d[48], d[49], d[50], d[51], d[52], d[53], d[54], d[55],
+   d[56], d[57], d[58], d[59], d[60], d[61], d[62], d[63]);
 }
 
 __device__ void QueuePair::post_wqe_amo(int pe, int32_t size, uintptr_t *laddr, uintptr_t *raddr, uint8_t opcode,
