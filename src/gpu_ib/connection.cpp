f04aad5cc99fc79b8884795fcadc3b6f16b43af3
Brandon Potter <brandon.potter@amd.com>
2025-04-15 08:28:15 -0500

    Add rkey/lkey check

--- src/gpu_ib/connection.cpp
+++ src/gpu_ib/connection.cpp
@@ -287,8 +287,11 @@ void Connection::init_gpu_qp_from_connection(QueuePair* gpu_qp, int conn_num) {
   uint32_t ctrl_qp_sq = (reinterpret_cast<uint32_t*>(sq))[1];
   gpu_qp->ctrl_qp_sq = ctrl_qp_sq & 0xFFFFFF;
   gpu_qp->ctrl_sig = (reinterpret_cast<uint64_t*>(sq))[1];
-  gpu_qp->rkey = (reinterpret_cast<uint32_t*>(sq))[6];
-  gpu_qp->lkey = (reinterpret_cast<uint32_t*>(sq))[9];
+
+  printf("host-side read of WR rkey %u\n", gpu_qp->rkey);
+  printf("backend->networkImpl.heap_rke[conn_num] %u\n", backend->networkImpl.heap_rkey[conn_num]);
+  printf("host-side read of WR lkey %u\n", gpu_qp->lkey);
+  printf("backend->networkImpl.heap_mr->lkey[i] %u\n", backend->networkImpl.heap_mr->lkey);
 }
 
 ibv_qp* Connection::create_qp(ibv_pd* pd, ibv_context* context, ibv_qp_init_attr_ex* qp_attr, ibv_cq* cq) {
