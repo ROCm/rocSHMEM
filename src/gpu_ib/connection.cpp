8ef245a39fbb8d13b851f99406a10d3a7d6df7cd
Brandon Potter <brandon.potter@amd.com>
2025-04-17 10:02:52 -0500

    Add print statuements

--- src/gpu_ib/connection.cpp
+++ src/gpu_ib/connection.cpp
@@ -286,6 +286,10 @@ void Connection::init_gpu_qp_from_connection(QueuePair* gpu_qp, int conn_num) {
   void* gpu_ptr{nullptr};
   rocm_memory_lock_to_fine_grain(qp_out.bf.reg, qp_out.bf.size, &gpu_ptr, hip_dev_id);
   gpu_qp->db.ptr = reinterpret_cast<uint64_t*>(gpu_ptr);
+  printf("gpu_qp %p", gpu_qp);
+  printf("qp_out.br.reg %p\n", qp_out.bf.reg);
+  printf("gpu_ptr %p on hip_dev_id %d\n", gpu_ptr, hip_dev_id);
+  printf("gpu_qp->db.ptr %p\n", gpu_qp->db.ptr);
 
 //  gpu_qp->rkey = (reinterpret_cast<uint32_t*>(sq))[6];
 //  gpu_qp->lkey = (reinterpret_cast<uint32_t*>(sq))[9];
