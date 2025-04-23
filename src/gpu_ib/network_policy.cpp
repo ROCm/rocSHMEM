d79fbf06ff4ac385c8ecc95e3a623d9847fe928a
Brandon Potter <brandon.potter@amd.com>
2025-04-23 04:30:33 -0500

    Fix THE OTHER bug

--- src/gpu_ib/network_policy.cpp
+++ src/gpu_ib/network_policy.cpp
@@ -136,7 +136,7 @@ void NetworkImpl::networkHostInit(GPUIBContext *ctx, int context_id) {
   CHECK_HIP(hipMalloc(&ctx->device_qp_proxy, sizeof(QueuePair) * num_pes));
   CHECK_HIP(hipMemset(ctx->device_qp_proxy, 0, sizeof(QueuePair) * num_pes));
   for (int i{0}; i < num_pes; i++) {
-    int offset = num_contexts * context_id + i;
+    int offset = num_pes * context_id + i;
     DPRINTF("num_pes %d num_contexts %d context_id %d i %d offset %d\n", num_pes, num_contexts, context_id, i, offset);
     DPRINTF("context %p shallow copying gpu_qps %p to local index %d\n", ctx, &gpu_qps[offset], offset);
     new (ctx->getQueuePair(i)) QueuePair(gpu_qps[offset]);
