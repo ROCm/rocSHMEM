81512cc10349b1bd4874d5963632bd28d9201a1d
Brandon Potter <BKP@users.noreply.github.com>
2025-05-20 15:57:17 -0500

    Check RMA functional test data in GPU kernel (#91) (#132)
    Co-authored-by: Yiltan <ytemucin@amd.com>

--- tests/functional_tests/workgroup_primitives.cpp
+++ tests/functional_tests/workgroup_primitives.cpp
@@ -1,5 +1,7 @@
 /******************************************************************************
- * Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -13,108 +15,82 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  *****************************************************************************/
 
-#include "primitive_tester.hpp"
+#include "workgroup_primitives.hpp"
 
 #include <rocshmem/rocshmem.hpp>
+
+#include <numeric>
 
 using namespace rocshmem;
 
 /******************************************************************************
  * DEVICE TEST KERNEL
  *****************************************************************************/
-__global__ void PrimitiveTest(int loop, int skip, long long int *start_time,
-                              long long int *end_time, char *source,
-                              char *dest, int size, TestType type,
-                              ShmemContextType ctx_type, int wf_size) {
+__global__ void WorkGroupPrimitiveTest(int loop, int skip,
+                                      long long int *start_time,
+                                      long long int *end_time, char *source,
+                                      char *dest, int size, TestType type,
+                                      ShmemContextType ctx_type) {
   __shared__ rocshmem_ctx_t ctx;
   int wg_id = get_flat_grid_id();
-  int t_id  = get_flat_block_id();
-  int wf_id = t_id / wf_size;
-  rocshmem_wg_ctx_create(&ctx);
+  rocshmem_wg_init();
+  rocshmem_wg_ctx_create(ctx_type, &ctx);
 
-  /**
-   * Shared array to capture the start time for each wavefront
-   * Max threads per block = 1024, wavefront size = 64 (in most GPUs)
-   * Maximum array size required = 1024/64 = 16
-   */
-  __shared__ long long int wf_start_time[16];
-
-  /**
-   * Calculate start index for each thread within the grid
-   */
-  uint64_t offset = size * get_flat_id();
+  // Calculate start index for each work group
+  uint64_t offset = size * wg_id;
   source += offset;
   dest += offset;
 
   for (int i = 0; i < loop + skip; i++) {
     if (i == skip) {
-      __syncthreads();
       // Ensures all RMA calls from the skip loops are completed
-      if(is_thread_zero_in_block()) {
+      if (is_thread_zero_in_block()) {
         rocshmem_ctx_quiet(ctx);
       }
       __syncthreads();
-      // Capture the start time of each wavefront to identify the earliest one
-      wf_start_time[wf_id] = wall_clock64();
+      start_time[wg_id] = wall_clock64();
     }
 
     switch (type) {
-      case PutTestType:
-        rocshmem_ctx_putmem(ctx, dest, source, size, 1);
+      case WGGetTestType:
+        rocshmem_ctx_getmem_wg(ctx, dest, source, size, 1);
         break;
-      case PutNBITestType:
-        rocshmem_ctx_putmem_nbi(ctx, dest, source, size, 1);
+      case WGGetNBITestType:
+        rocshmem_ctx_getmem_nbi_wg(ctx, dest, source, size, 1);
         break;
-      case PTestType:
-        for (int s = 0; s < size; s++) {
-          char val = source[s];
-          rocshmem_ctx_char_p(ctx, &dest[s], val, 1);
-        }
+      case WGPutTestType:
+        rocshmem_ctx_putmem_wg(ctx, dest, source, size, 1);
+        break;
+      case WGPutNBITestType:
+        rocshmem_ctx_putmem_nbi_wg(ctx, dest, source, size, 1);
         break;
       default:
         break;
     }
   }
 
-  __syncthreads();
-  if(is_thread_zero_in_block()) {
+  if (is_thread_zero_in_block()) {
     rocshmem_ctx_quiet(ctx);
-  }
-
-  /**
-   * End time of the last wavefront is recorded by overwriting
-   * the value previously set by earlier wavefronts.
-   */
-  end_time[wg_id] = wall_clock64();
-
-  // Find the earliest start time
-  int num_wfs = (get_flat_block_size() - 1 ) / wf_size + 1;
-  for (int i = num_wfs / 2; i > 0; i >>= 1 ) {
-    if(t_id < i) {
-      wf_start_time[t_id] = min(wf_start_time[t_id], wf_start_time[t_id + i]);
-    }
-  }
-  __syncthreads();
-
-  if (t_id == 0) {
-    start_time[wg_id] = wf_start_time[0];
+    end_time[wg_id] = wall_clock64();
   }
 
   rocshmem_wg_ctx_destroy(&ctx);
+  rocshmem_wg_finalize();
 }
 
 /******************************************************************************
  * HOST TESTER CLASS METHODS
  *****************************************************************************/
-PrimitiveTester::PrimitiveTester(TesterArguments args) : Tester(args) {
-  size_t buff_size = args.max_msg_size * args.wg_size * args.num_wgs;
+WorkGroupPrimitiveTester::WorkGroupPrimitiveTester(TesterArguments args)
+    : Tester(args) {
+  size_t buff_size = args.max_msg_size * args.num_wgs;
   source = (char *)rocshmem_malloc(buff_size);
   dest = (char *)rocshmem_malloc(buff_size);
 
@@ -135,36 +111,35 @@ PrimitiveTester::PrimitiveTester(TesterArguments args) : Tester(args) {
   }
 }
 
-PrimitiveTester::~PrimitiveTester() {
+WorkGroupPrimitiveTester::~WorkGroupPrimitiveTester() {
   rocshmem_free(source);
   rocshmem_free(dest);
 }
 
-void PrimitiveTester::resetBuffers(uint64_t size) {
-  size_t buff_size = size * args.wg_size * args.num_wgs;
+void WorkGroupPrimitiveTester::resetBuffers(uint64_t size) {
+  size_t buff_size = size * args.num_wgs;
   memset(dest, '1', buff_size);
 }
 
-void PrimitiveTester::launchKernel(dim3 gridSize, dim3 blockSize, int loop,
-                                   uint64_t size) {
+void WorkGroupPrimitiveTester::launchKernel(dim3 gridSize, dim3 blockSize,
+                                           int loop, uint64_t size) {
   size_t shared_bytes = 0;
 
-  hipLaunchKernelGGL(PrimitiveTest, gridSize, blockSize, shared_bytes, stream,
-                     loop, args.skip, start_time, end_time, source, dest,
-                     size, _type, _shmem_context, wf_size);
+  hipLaunchKernelGGL(WorkGroupPrimitiveTest, gridSize, blockSize, shared_bytes,
+                     stream, loop, args.skip, start_time, end_time,
+                     source, dest, size, _type, _shmem_context);
 
-  num_msgs = (loop + args.skip) * gridSize.x * blockSize.x;
-  num_timed_msgs = loop * gridSize.x * blockSize.x;
+  num_msgs = (loop + args.skip) * gridSize.x;
+  num_timed_msgs = loop * gridSize.x;
 }
 
-void PrimitiveTester::verifyResults(uint64_t size) {
-  int check_id =
-      (_type == GetTestType || _type == GetNBITestType || _type == GTestType)
-          ? 0
-          : 1;
+void WorkGroupPrimitiveTester::verifyResults(uint64_t size) {
+  int check_id = (_type == WGGetTestType || _type == WGGetNBITestType)
+                     ? 0
+                     : 1;
 
   if (args.myid == check_id) {
-    size_t buff_size = size * args.wg_size * args.num_wgs;
+    size_t buff_size = size * args.num_wgs;
     size_t verify_wg_size = std::min((size_t) 1024, buff_size);
     size_t verify_num_wgs = buff_size / verify_wg_size;
 
@@ -173,7 +148,7 @@ void PrimitiveTester::verifyResults(uint64_t size) {
     CHECK_HIP(hipStreamSynchronize(stream));
 
     if (*verification_error) {
-      for (uint64_t i = 0; i < buff_size; i++) {
+      for (size_t i = 0; i < buff_size; i++) {
         if (dest[i] != source[i]) {
           std::cerr << "Data validation error at idx " << i << std::endl;
           std::cerr << " Got " << dest[i] << ", Expected "
