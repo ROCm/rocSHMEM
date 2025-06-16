787fe311b5744e0c532099bb797acc949f3324e8
Avinash Kethineedi <avinash.kethineedi@amd.com>
2025-06-16 14:08:10 -0500

    Update tests/functional_tests/sync_all_tester.cpp
    Co-authored-by: Yiltan <ytemucin@amd.com>

--- tests/functional_tests/sync_all_tester.cpp
+++ tests/functional_tests/sync_all_tester.cpp
@@ -61,7 +61,7 @@ __global__ void SyncAllTest(int loop, int skip, long long int *start_time,
           if(wf_id == 0) {
             /**
               * The function `rocshmem_sync_all_wave` should be called from only
-              * one thread within the grid to avoid unintended behavior.
+              * one thread within the grid to avoid undefined behavior.
               */
             rocshmem_sync_all_wave();
           }
