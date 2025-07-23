e907c09ad38d45caa1fdbb2f8e3f332893018ad5
avinashkethineedi <avinash.kethineedi@amd.com>
2025-07-23 21:16:51 +0000

    Copy IPC pointers to host memory in RO host context

--- src/reverse_offload/context_ro_host.cpp
+++ src/reverse_offload/context_ro_host.cpp
@@ -43,13 +43,20 @@ __host__ ROHostContext::ROHostContext(Backend *backend, long options)
   context_window_info = dynamic_cast<WindowInfoMPI*>(host_interface->acquire_window_context());
 
   int *pes_with_ipc_avail = new int[backend->ipcImpl.shm_size];
+  char** ipc_bases = new char*[b->ipcImpl.shm_size];
 
   CHECK_HIP(hipMemcpy(pes_with_ipc_avail,
                   backend->ipcImpl.pes_with_ipc_avail,
                   backend->ipcImpl.shm_size * sizeof(int),
                   hipMemcpyDeviceToHost));
 
+  CHECK_HIP(hipMemcpy(ipc_bases,
+                  backend->ipcImpl.ipc_bases,
+                  backend->ipcImpl.shm_size * sizeof(char *),
+                  hipMemcpyDeviceToHost));
+
   ipcImpl_.pes_with_ipc_avail = pes_with_ipc_avail;
+  ipcImpl_.ipc_bases = ipc_bases;
   ipcImpl_.shm_size = backend->ipcImpl.shm_size;
   ipcImpl_.shm_rank = backend->ipcImpl.shm_rank;
 }
@@ -57,6 +64,7 @@ __host__ ROHostContext::ROHostContext(Backend *backend, long options)
 __host__ ROHostContext::~ROHostContext() {
   // host_interface->release_window_context(context_window_info);
   delete[] ipcImpl_.pes_with_ipc_avail;
+  delete[] ipcImpl_.ipc_bases;
 }
 
 __host__ void ROHostContext::putmem_nbi(void *dest, const void *source,
