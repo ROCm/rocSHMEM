9ac37ecb9b1502f54e63c1e243c8741f699e1f26
Aurelien Bouteiller <abouteil@amd.com>
2025-09-11 09:31:27 -0400

    Make it possible to test RMA GET and PUT separately DISABLE_GET removed from ALL, idea is that the CI scripts will invoke a subset that is known to work rather than ALL

--- scripts/functional_tests/driver.sh
+++ scripts/functional_tests/driver.sh
@@ -286,9 +286,7 @@ TestRMAGet() {
 
 TestRMA() {
   TestRMAPut
-  if [ "0" == "$ROCSHMEM_DRIVER_DISABLE_GET" ]; then
-    TestRMAGet
-  fi
+  TestRMAGet
 }
 
 TestAMO() {
@@ -645,7 +643,6 @@ LOG_DIR=$3
 HOSTFILE=$4
 
 DRIVER_RETURN_STATUS=0
-ROCSHMEM_DRIVER_DISABLE_GET="${ROCSHMEM_DRIVER_DISABLE_GET:-1}"
 
 ValidateInput $#
 ValidateLogDir $LOG_DIR
@@ -663,6 +660,12 @@ case $TEST in
     ;;
   *"rma")
     TestRMA
+    ;;
+  *"put")
+    TestRMAPut
+    ;;
+  *"get")
+    TestRMAGet
     ;;
   *"amo")
     TestAMO
