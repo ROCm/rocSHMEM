ac20795c08331b1d83153c042343e39b4018cd32
Yiltan <yiltan@amd.com>
2025-10-29 15:13:58 +0000

    fix rounding error

--- src/gda/context_gda_device_coll.cpp
+++ src/gda/context_gda_device_coll.cpp
@@ -77,7 +77,7 @@ __device__ void GDAContext::internal_direct_barrier_wg(int pe, int PE_start,
 
   if (pe == PE_start) {
     int wf_id = get_flat_block_id() / WF_SIZE;
-    int wf_count = get_flat_block_size() / WF_SIZE;
+    int wf_count = (int) ceil((double)get_flat_block_size() / (double)WF_SIZE);
     bool wf_leader = 0 == get_active_lane_num();
 
     // Go through all PE offsets (except current offset = 0)
