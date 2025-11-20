ef9bd44efe499e87d544cf31e6d25ed40b8dc28d
Edgar Gabriel <Edgar.Gabriel@amd.com>
2025-11-20 19:43:24 +0000

    add fix for gfx1201
    add the same fix for gfx1201 that was introduced for gfx1100

--- src/assembly.hpp
+++ src/assembly.hpp
@@ -221,15 +221,16 @@ __device__ __forceinline__ void store_asm(uint8_t* val, uint8_t* dst,
                                           int size) {
   switch (size) {
     case 2: {
-      int16_t val16{*(reinterpret_cast<int16_t*>(val))};
 #if defined(__gfx906__)
 #endif
 #if defined(__gfx908__)
 #endif
 #if defined(__gfx90a__)
+      int16_t val16{*(reinterpret_cast<int16_t*>(val))};
       asm volatile("flat_store_short %0 %1 glc slc" : : "v"(dst), "v"(val16));
 #endif
 #if defined(__gfx942__) || defined(__gfx950__)
+      int16_t val16{*(reinterpret_cast<int16_t*>(val))};
       asm volatile("flat_store_short %0 %1 sc0 sc1" : : "v"(dst), "v"(val16));
 #endif
 #if defined(__gfx1100__)
@@ -237,7 +238,8 @@ __device__ __forceinline__ void store_asm(uint8_t* val, uint8_t* dst,
       asm volatile("flat_store_short %0 %1 glc slc" : : "v"(dst), "v"(val32));
 #endif
 #if defined(__gfx1201__)
-      asm volatile("flat_store_b16 %0 %1 scope:SCOPE_SYS" : : "v"(dst), "v"(val16));
+      int32_t val32{*(reinterpret_cast<int32_t*>(val))};
+      asm volatile("flat_store_b16 %0 %1 scope:SCOPE_SYS" : : "v"(dst), "v"(val32));
 #endif
       break;
     }
