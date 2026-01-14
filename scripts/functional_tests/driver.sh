5704056e31b3b5095ef78cccf0a02ed9f8f2fd6f
Aurelien Bouteiller <abouteil@amd.com>
2026-01-14 10:41:35 -0500

    Add flood tester to the testing script

--- scripts/functional_tests/driver.sh
+++ scripts/functional_tests/driver.sh
@@ -116,6 +116,12 @@ declare -A TEST_NUMBERS=(
   ["putmem_on_stream"]="80"
   ["putmem_signal_on_stream"]="81"
   ["signal_wait_until_on_stream"]="82"
+  ["flood_put"]="83"
+  ["flood_putnbi"]="84"
+  ["flood_p"]="85"
+  ["flood_get"]="86"
+  ["flood_getnbi"]="87"
+  ["flood_g"]="88"
 )
 
 ExecTest() {
@@ -473,6 +479,16 @@ TestOther() {
   ExecTest  "pingall"          2       1            1
   ExecTest  "pingall"          2       8            1
   ExecTest  "pingall"          2       32           1
+
+  ExecTest  "flood_put"        2       64           1024
+  ExecTest  "flood_get"        2       64           1024
+
+  ExecTest  "flood_put"        8       64           1024
+  ExecTest  "flood_putnbi"     8       64           1024
+  ExecTest  "flood_p"          8       64           1024
+  ExecTest  "flood_get"        8       64           1024
+  ExecTest  "flood_getnbi"     8       64           1024
+  ExecTest  "flood_g"          8       64           1024
 
   # This test requires more contexts than workgroups
   export ROCSHMEM_MAX_NUM_CONTEXTS=1024
