429e1f2a137fb2df2128cb710f774e0a85fd3d45
Aurelien Bouteiller <abouteil@amd.com>
2026-01-20 10:11:41 -0500

    If we find heatmapcoll do not prefer *coll

--- scripts/functional_tests/driver.sh
+++ scripts/functional_tests/driver.sh
@@ -768,6 +768,16 @@ ValidateInput $#
 ValidateLogDir $LOG_DIR
 
 case $TEST in
+  *"heatmaprma")
+    TestHeatMapRMA
+    ;;
+  *"heatmapcoll")
+    TestHeatMapColl
+    ;;
+  *"heatmap")
+    TestHeatMapRMA
+    TestHeatMapColl
+    ;;
   *"gda")
     TestGDA
     ;;
@@ -805,16 +815,6 @@ case $TEST in
     ;;
   *"other")
     TestOther
-    ;;
-  *"heatmaprma")
-    TestHeatMapRMA
-    ;;
-  *"heatmapcoll")
-    TestHeatMapColl
-    ;;
-  *"heatmap")
-    TestHeatMapRMA
-    TestHeatMapColl
     ;;
   *)
     ##############################################################################
