ad457c2a06f4f60c13a1aff690e4dd1cb43334db
Yiltan <yiltan@amd.com>
2026-01-20 14:26:25 +0000

    working alltoallv

--- src/gda/context_gda_tmpl_device.hpp
+++ src/gda/context_gda_tmpl_device.hpp
@@ -626,20 +626,58 @@ __device__ void GDAContext::alltoallv(rocshmem_team_t team,
     abort();
   }
 
-  GDATeam *team_obj = reinterpret_cast<GDATeam*>(team);
+  GDATeam *team_obj = reinterpret_cast<GDATeam *>(team);
 
+  int pe_start = team_obj->tinfo_wrt_world->pe_start;
   int pe_size = team_obj->num_pes;
-  T *staging_buffer = reinterpret_cast<T*>(team_obj->pWrk);
+  int stride = team_obj->tinfo_wrt_world->stride;
+  long *pSync = team_obj->alltoall_pSync;
+  int my_pe_in_team = team_obj->my_pe;
+  uint64_t alltoall_pSync_offset = (team_obj->alltoall_sequence_number % 2) * pe_size;
+  T *tmp_buf = reinterpret_cast<T*>(team_obj->pWrk);
+  int tmp_buf_off = (ROCSHMEM_REDUCE_MIN_WRKDATA_SIZE * sizeof(double)) / (pe_size * sizeof(T));
 
-  alltoall_linear_thread_puts(team, staging_buffer, source, source_nelems[0]);
+  int tid = get_flat_block_id();
+  int step_size = min(get_flat_block_size(), WF_SIZE);
+
+  // Have each PE put their designated data to the other PEs
+  for (int j = tid; j < pe_size; j+= step_size) {
+    int dest_pe = team_obj->get_pe_in_world(j);
+    uint64_t base_heap_offset = base_heap[dest_pe] - base_heap[my_pe];
+
+    size_t nelems = source_nelems[dest_pe] * sizeof(T);
+    T* src = (T*)((char*)source + source_displs[j]);
+    T* dst = (T*)((char*)&tmp_buf[my_pe * tmp_buf_off] + base_heap_offset);
+    char* amo_dst = ((char*)&pSync[alltoall_pSync_offset + my_pe_in_team] + base_heap_offset);
+
+    qps[dest_pe].put_nbi_single(dst, src, nelems, false);
+    qps[dest_pe].atomic_nofetch_single(amo_dst, 1);
+  }
+
+  // wait until everyone has obtained their designated data
+  for (int j = tid; j < pe_size; j+= step_size) {
+    int dest_pe = team_obj->get_pe_in_world(j);
+
+    volatile long *vol_ivars = &pSync[alltoall_pSync_offset + dest_pe];
+    while (uncached_load(vol_ivars) != 1) { }
+
+    pe_quiet_single(dest_pe);
+
+    pSync[alltoall_pSync_offset + dest_pe] = ROCSHMEM_SYNC_VALUE;
+  }
 
   // Copy out of staging bufer
   __syncthreads();
 
   for (int j = 0; j < pe_size; j++) {
     size_t nelems = dest_nelems[j];
-    int idx = j * nelems;
-    memcpy_wg(&dest[idx], &staging_buffer[idx], nelems * sizeof(T));
+    T* dst = (T*)((char*) dest + dest_displs[j]);
+    T* src = (T*)((char*) &tmp_buf[j * tmp_buf_off]);
+    memcpy_wg(dst, src, nelems * sizeof(T));
+  }
+
+  if (is_thread_zero_in_block()) {
+    team_obj->alltoall_sequence_number++;
   }
 }
 
